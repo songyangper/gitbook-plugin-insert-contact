1966d003c92bba0cd0bec2f4cbfffd9f9d668e97
songyang@yunlizhihui <songyang@yunlizhihui.com>
2020-07-10 14:26:50 +0800

    commit稳定版

--- assets/contact.js
+++ assets/contact.js
@@ -1,18 +1,16 @@
 require(['gitbook', 'jQuery'], function (gitbook, $) {
-    var showAuthor = ''
     var author = ''
-    var insertLogo = function (showAuthor) {
-        if (showAuthor) {
-            author = this.options.author;
+    var insertLogo = function (author) {
+        if (author) {
+            $('.book-summary').children().eq(1).before('<div class="book-author">' + '作者：' + author + '</div>')
         }
-        $('.book-summary').children().eq(1).before('<div class="book-author">' + '作者' + author + '</div>')
     }
 
     gitbook.events.bind('start', function (e, config) {
-        showAuthor = config['insert-contact']['showAuthor'];
+        author = config['insert-contact']['author']
     })
 
     gitbook.events.bind("page.change", function() {
-        insertLogo(showAuthor)
+        insertLogo(author)
     })
 })
