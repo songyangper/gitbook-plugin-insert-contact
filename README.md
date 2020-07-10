cc40ae30b0d5046b3d26319f5fabf68c0d4539a5
songyang@yunlizhihui <songyang@yunlizhihui.com>
2020-07-10 14:57:05 +0800

    commit1.0.6

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # gitbook-plugin-insert-contact
+
+NPM package [here](https://www.npmjs.com/package/gitbook-plugin-insert-contact)
+
+```
+npm i gitbook-plugin-insert-contact
+```
+gitbook plugin insert contact
+
+```json
+{
+    "plugins": ["insert-contact", "another plugin 1", "another plugin 2"]
+}
+```
+
+You will also need to provide author and style in your `book.json`:
+
+```json
+{
+    "plugins": ["insert-contact", "another plugin"],
+    "pluginsConfig": {
+        "insert-contact": {
+            "author": "***",
+            "style": "background: none; max-height: 30px; min-height: 30px; color: #ddd"
+        }
+    }
+}
+```
--- assets/contact.js
+++ assets/contact.js
@@ -1,16 +1,18 @@
 require(['gitbook', 'jQuery'], function (gitbook, $) {
     var author = ''
-    var insertLogo = function (author) {
+    var style = ''
+    var insertLogo = function (author, style) {
         if (author) {
-            $('.book-summary').children().eq(1).before('<div class="book-author">' + '作者：' + author + '</div>')
+            $('.book-summary').children().eq(1).before('<div class="book-author" style="' + style + '">' + '作者：' + author + '</div>')
         }
     }
 
     gitbook.events.bind('start', function (e, config) {
         author = config['insert-contact']['author']
+        style = config['insert-contact']['style']
     })
 
     gitbook.events.bind("page.change", function() {
-        insertLogo(author)
+        insertLogo(author, style)
     })
 })
